4ef81d212da70097ba7d595a71110ab6f0845a93
Coalman <coalman@github>
2012-10-24 00:39:38 -0400

    Made an extremely rough food implementation. Need to refactor.

--- Coalman/food_element.dart
+++
@@ -1,5 +0,0 @@
-
-class FoodElement extends PElement
-{
-
-}
--- main.dart
+++ main.dart
@@ -1,30 +1,39 @@
-#import('dart:html');
-
-#import("graphics.dart");
-
+#import("dart:html");
 #import('dart:math', prefix:'math');
 #import('package:bot/bot.dart');
 #import('package:bot/html.dart');
 #import('package:bot/retained.dart');
+#import("lib/graphics.dart");
 
-CanvasRenderingContext2D context;
+CanvasRenderingContext2D ctx;
 Stage stage;
+FoodElement food;
 
 void main() {
   final CanvasElement canvas = query("#displayCanvas");
-  context = canvas.context2d;
-  
+  ctx = canvas.context2d;
+
   //drawer = new GridDrawer(canvas);
   stage = createStage(canvas);
+
+  window.requestAnimationFrame(loop);
 }
 
 Stage createStage(CanvasElement canvas)
 {
   PCanvas root = new PCanvas(canvas.width, canvas.height);
-  
-  GridView grid = new GridView(canvas.width ~/ 32, canvas.height ~/ 32, new MockGridData());
-  root.addElement(grid);
-  
-  stage = new Stage(canvas, root);
+
+  food = new FoodElement.fromUrl(24, 24);
+  root.addElement(food);
+  root.setCenter(food, new Coordinate(50, 50));
+
+  return new Stage(canvas, root);
+}
+
+void loop(int highResTime)
+{
+  food.doAnim(highResTime);
   stage.draw();
+
+  window.requestAnimationFrame(loop);
 }
